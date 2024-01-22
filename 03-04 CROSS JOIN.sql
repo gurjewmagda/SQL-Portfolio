247ba752e9cbd09e9f705dde30bdba414ce7fdd7
gurjewmagda <116095630+gurjewmagda@users.noreply.github.com>
2024-01-22 21:30:32 +0100

    Update 03-04 CROSS JOIN.sql

--- 03-04 CROSS JOIN.sql
+++ 03-04 CROSS JOIN.sql
@@ -1,4 +1,3 @@
-select * from product_groups
 
 select 
 	pg.group_id
@@ -6,3 +5,16 @@ select
 	,p.group_id as original_group
 from products p
 cross join product_groups pg
+
+|group_id|product_id|original_group|
+|--------|----------|--------------|
+|17      |1,847     |17            |
+|16      |1,847     |17            |
+|15      |1,847     |17            |
+|14      |1,847     |17            |
+|13      |1,847     |17            |
+|12      |1,847     |17            |
+|11      |1,847     |17            |
+|10      |1,847     |17            |
+|9       |1,847     |17            |
+|8       |1,847     |17            |
