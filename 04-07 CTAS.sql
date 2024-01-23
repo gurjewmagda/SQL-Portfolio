5d1ade1a9d8d08cda0bf8ed6dbb75bac01c86eda
gurjewmagda <116095630+gurjewmagda@users.noreply.github.com>
2024-01-23 09:35:32 +0100

    Update 04-07 CTAS.sql

--- 04-07 CTAS.sql
+++ 04-07 CTAS.sql
@@ -2,7 +2,7 @@ create table orders_2020 as
 select * from orders
 where year(order_date)=2020
 
--- chce stworzyc kopie tabeli to prosta komenda:
+-- create table as select
 
 create table products_copy as
 select* from products
