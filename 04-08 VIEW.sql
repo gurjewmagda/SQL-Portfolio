96767075e5427cdf45d3cacf4ceecd446e4a1ad1
gurjewmagda <116095630+gurjewmagda@users.noreply.github.com>
2024-01-23 09:38:29 +0100

    Update 04-08 VIEW.sql

--- 04-08 VIEW.sql
+++ 04-08 VIEW.sql
@@ -1,12 +1,27 @@
 select * from orders 
 where order_date>= date('2022-01-01');
--- tworzymy nasz widok
+
+-- create a View
 
 create view vw_orders_after_2022 as
 select * from orders 
 where order_date >= date('2022-01-01')
 
 select * from vw_orders_after_2022
+	
+|order_id|order_date|
+|--------|----------|
+|362     |2022-01-04|
+|382     |2022-01-20|
+|396     |2022-01-17|
+|427     |2022-01-12|
+|429     |2022-01-04|
+|453     |2022-01-04|
+|454     |2022-01-23|
+|504     |2022-01-06|
+|513     |2022-01-19|
+|514     |2022-01-03|
+
 
 create or replace view vw_orders_after_2022 as
 select 
