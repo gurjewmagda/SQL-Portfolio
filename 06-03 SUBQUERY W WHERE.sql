3869119a7a32fd604b702109d4fbb7ffb45580c0
gurjewmagda <116095630+gurjewmagda@users.noreply.github.com>
2024-01-23 10:07:14 +0100

    Update 06-03 SUBQUERY W WHERE.sql

--- 06-03 SUBQUERY W WHERE.sql
+++ 06-03 SUBQUERY W WHERE.sql
@@ -1,3 +1,5 @@
+/* Only states with number of customers >100 */
+
 select 
 	o.delivery_state 
 	,o.shipping_mode 
@@ -16,7 +18,7 @@ inner join
 	) ds on ds.delivery_state = o.delivery_state 
 where
 	o.delivery_state in 
-		(
+		(					
 		select delivery_state
 		from orders
 		group by 1
@@ -25,6 +27,7 @@ where
 group by 1, 2, 3, 4
 order by 1, 2
 
+======================================================
 
 select
 	delivery_state
@@ -32,3 +35,17 @@ select
 from orders
 group by 1
 having count(distinct customer_id)>100
+
+|delivery_state|customers|
+|--------------|---------|
+|California    |577      |
+|Florida       |181      |
+|Illinois      |237      |
+|Michigan      |106      |
+|New York      |415      |
+|North Carolina|122      |
+|Ohio          |202      |
+|Pennsylvania  |257      |
+|Texas         |370      |
+|Virginia      |107      |
+|Washington    |224      |
