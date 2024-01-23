fb8ab100c8b7cc24db5e47b7a2e2f8aa51791fcf
gurjewmagda <116095630+gurjewmagda@users.noreply.github.com>
2024-01-23 10:24:52 +0100

    Update 07-02 CASE WHEN.sql

--- 07-02 CASE WHEN.sql
+++ 07-02 CASE WHEN.sql
@@ -2,9 +2,9 @@ select
 	customer_id 
 	,count(order_id) as orders
 from orders
-group by 1; -- najpierw mamy to
+group by 1; -- first step
 
-/* teraz dodamy subquery bo chcemy poklasyfikowac klientów jako np loyal, new itp na podstawie ilosci zamowien*/
+/* add subquery to clasify customers as for example loyal, new on the basis of number of orders*/
 
 select
 	o.*
@@ -21,7 +21,30 @@ select
 	,count(order_id) as orders
 from orders
 group by 1
-) o ;-- uwaga !! dał new i regular tylko. jak drugi warunek spelniony to nie przechodzi do nastepnego
+) o ;					-- uwaga !! dał new i regular tylko. jak drugi warunek spelniony to nie przechodzi do nastepnego
+
+|customer_id|orders|customer_type|
+|-----------|------|-------------|
+|166        |3     |Regular      |
+|262        |5     |Regular      |
+|728        |6     |Regular      |
+|111        |8     |Regular      |
+|25         |4     |Regular      |
+|361        |7     |Regular      |
+|353        |7     |Regular      |
+|620        |12    |Regular      |
+|41         |5     |Regular      |
+|815        |5     |Regular      |
+|440        |12    |Regular      |
+|707        |7     |Regular      |
+|272        |10    |Regular      |
+|281        |8     |Regular      |
+|761        |9     |Regular      |
+|505        |11    |Regular      |
+|323        |2     |Regular      |
+|727        |7     |Regular      |
+|475        |8     |Regular      |
+|640        |7     |Regular      |
 
 
 select
@@ -40,3 +63,26 @@ select
 from orders
 group by 1
 ) o
+
+|customer_id|orders|customer_type|
+|-----------|------|-------------|
+|166        |3     |Regular      |
+|262        |5     |Loyal        |
+|728        |6     |Loyal        |
+|111        |8     |Loyal        |
+|25         |4     |Regular      |
+|361        |7     |Loyal        |
+|353        |7     |Loyal        |
+|620        |12    |Very Loyal   |
+|41         |5     |Loyal        |
+|815        |5     |Loyal        |
+|440        |12    |Very Loyal   |
+|707        |7     |Loyal        |
+|272        |10    |Very Loyal   |
+|281        |8     |Loyal        |
+|761        |9     |Loyal        |
+|505        |11    |Very Loyal   |
+|323        |2     |Regular      |
+|727        |7     |Loyal        |
+|475        |8     |Loyal        |
+|640        |7     |Loyal        |
